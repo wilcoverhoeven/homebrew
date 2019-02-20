dca1d162e4e4076111660231da9a9d6e3f9e8eda
Wilco Verhoeven <wilcoverhoeven@gmail.com>
2019-02-20 13:24:15 +0100

    added retina-display-menu cask

---
+++ Casks/retina-display-menu.rb
@@ -0,0 +1,10 @@
+cask 'retina-display-menu' do
+  version '2.3.1'
+  sha256 :no_check
+
+  url 'https://github.com/usr-sse2/RDM/releases/download/{version}/RDM.zip'
+  name 'retina-display-menu'
+  homepage 'https://github.com/usr-sse2/RDM'
+
+  app 'RDM.app'
+end
