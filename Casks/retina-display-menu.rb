42c84b5f3272a83d2afbf55dffd50301ec027945
Wilco Verhoeven <wilcoverhoeven@gmail.com>
2019-02-20 13:57:15 +0100

    retina-display-menu cask test hardcoded version number

--- Casks/retina-display-menu.rb
+++ Casks/retina-display-menu.rb
@@ -1,6 +1,6 @@
 cask 'retina-display-menu' do
   version '2.3.1'
-  sha256 :no_check
+  sha256 'dd03c374818214d4f3e07307f722a8543aec16f6da50cc39d60717bd503acf7b'
 
   url 'https://github.com/usr-sse2/RDM/releases/download/2.3.1/RDM.zip'
   appcast 'https://github.com/usr-sse2/RDM/releases.atom'
