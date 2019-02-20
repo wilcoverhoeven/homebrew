fa854c779c67fdbcfda1cf7bf02519743063ca8c
Wilco Verhoeven <wilcoverhoeven@gmail.com>
2019-02-20 13:58:54 +0100

    retina-display-menu cask reverted back to variable version number

--- Casks/retina-display-menu.rb
+++ Casks/retina-display-menu.rb
@@ -2,7 +2,7 @@ cask 'retina-display-menu' do
   version '2.3.1'
   sha256 'dd03c374818214d4f3e07307f722a8543aec16f6da50cc39d60717bd503acf7b'
 
-  url 'https://github.com/usr-sse2/RDM/releases/download/2.3.1/RDM.zip'
+  url "https://github.com/usr-sse2/RDM/releases/download/#{version}/RDM.zip"
   appcast 'https://github.com/usr-sse2/RDM/releases.atom'
   name 'retina-display-menu'
   homepage 'https://github.com/usr-sse2/RDM'
