03a9803521c0b15467de35d7ab5bd4f7e01b01b4
Wilco Verhoeven <wilcoverhoeven@gmail.com>
2019-02-20 13:34:54 +0100

    retina-display-menu cask fixed url... needed double qoutes...

--- Casks/retina-display-menu.rb
+++ Casks/retina-display-menu.rb
@@ -2,7 +2,7 @@ cask 'retina-display-menu' do
   version '2.3.1'
   sha256 :no_check
 
-  url 'https://github.com/usr-sse2/RDM/releases/download/v#{version}/RDM.zip'
+  url "https://github.com/usr-sse2/RDM/releases/download/#{version}/RDM.zip"
   appcast 'https://github.com/usr-sse2/RDM/releases.atom'
   name 'retina-display-menu'
   homepage 'https://github.com/usr-sse2/RDM'
